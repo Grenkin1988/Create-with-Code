b55ee0cacab740faa021a21f18cbd45f86fff412
Grenkin1988 <kruglovgleb@gmail.com>
2020-04-22 20:15:25 +0300

    Lesson 1.2

---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+    }
+}
