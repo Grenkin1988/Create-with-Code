857199fc1e7d4518e602f0f90363e56b1cb748f2
Grenkin1988 <kruglovgleb@gmail.com>
2020-04-23 21:56:26 +0300

    Lesson 2.1 - Player Positioning

---
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float _horizontalInput;
+    [SerializeField]
+    private float _movementSpeed = 10.0f;
+    [SerializeField]
+    private float _xBorderValue = 10;
+
+    private void Update()
+    {
+        var current = transform.position;
+        if(current.x < -_xBorderValue) {
+            transform.position = new Vector3(-_xBorderValue, current.y, current.z);
+        }
+        if (current.x > _xBorderValue) {
+            transform.position = new Vector3(_xBorderValue, current.y, current.z);
+        }
+        _horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * _horizontalInput * _movementSpeed);
+    }
+}
