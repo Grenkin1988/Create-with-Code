a25efe6f2bb2dc9e065f9311f2b540c1620053c8
Grenkin1988 <kruglovgleb@gmail.com>
2020-04-23 22:42:06 +0300

    Lesson 2.3 - Random Animal Stampede

---
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+    [SerializeField]
+    private GameObject[] _animalPrefabs;
+    [SerializeField]
+    private float _spawnPositionZ = 20;
+    [SerializeField]
+    private float _spawnPositionXRange = 20;
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.S) && _animalPrefabs?.Length > 0) {
+
+            int index = Random.Range(0, _animalPrefabs.Length);
+            var prefab = _animalPrefabs[index];
+
+            var spawnPosition = new Vector3(Random.Range(-_spawnPositionXRange, _spawnPositionXRange), 0, _spawnPositionZ);
+
+            Instantiate(prefab, spawnPosition, prefab.transform.rotation);
+        }
+    }
+}
