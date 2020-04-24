286b96b6c3bb83a899e1124dbbfa7a07be0bcda6
Grenkin1988 <kruglovgleb@gmail.com>
2020-04-24 22:56:07 +0300

    Lesson 3.3 - Don't Just Stand There

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@
 public class PlayerController : MonoBehaviour {
     private Rigidbody _rigidbody;
     private bool isOnGround = true;
-    
+    private Animator _playerAnim;
 
     [SerializeField]
     private float _jumpForce = 12;
@@ -12,14 +12,20 @@ public class PlayerController : MonoBehaviour {
 
     public bool GameOver { get; private set; }
 
-    private void Start() {
+    private void Awake() {
         _rigidbody = GetComponent<Rigidbody>();
+        _playerAnim = GetComponent<Animator>();
+    }
+
+    private void Start() {
+        
         Physics.gravity *= _gravityModifier;
     }
 
     private void Update() {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !GameOver) {
             _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+            _playerAnim.SetTrigger("Jump_trig");
             isOnGround = false;
         }
     }
@@ -31,6 +37,8 @@ public class PlayerController : MonoBehaviour {
         } else if (collision.gameObject.CompareTag("Obstacle")) {
             GameOver = true;
             Debug.Log("Game Over!");
+            _playerAnim.SetBool("Death_b", true);
+            _playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
