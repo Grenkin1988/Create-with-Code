fdb295afe2660273872031511794971c4d87ec8a
Grenkin1988 <kruglovgleb@gmail.com>
2020-04-25 16:49:43 +0300

    Lesson 4.3 - PowerUp and CountDown

--- Prototype 4/Assets/Scripts/PlayerController.cs
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,20 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     private Rigidbody playerRigidbody;
     private GameObject focalPoint;
 
     [SerializeField]
+    private GameObject powerupIndicator;
+
+    [SerializeField]
     private float moveSpped = 10;
+    private bool hasPowerup;
+    [SerializeField]
+    private float powerUpStrength = 15;
+    [SerializeField]
+    private float powerUpDuration = 7;
 
     private void Awake() {
         playerRigidbody = GetComponent<Rigidbody>();
@@ -18,5 +27,31 @@ public class PlayerController : MonoBehaviour {
     private void Update() {
         float forward = Input.GetAxis("Vertical");
         playerRigidbody.AddForce(focalPoint.transform.forward * moveSpped * forward);
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Powerup")) {
+            hasPowerup = true;
+            powerupIndicator.SetActive(hasPowerup);
+            Destroy(other.gameObject);
+            StartCoroutine(nameof(PowerupCountdownRoutine));
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (hasPowerup && collision.gameObject.CompareTag("Enemy")) {
+            var enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            var awayFromPlayer = collision.gameObject.transform.position - transform.position;
+
+            enemyRigidbody.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+        }
+    }
+
+    private IEnumerator PowerupCountdownRoutine() {
+        yield return new WaitForSeconds(powerUpDuration);
+        hasPowerup = false;
+        powerupIndicator.SetActive(hasPowerup);
     }
 }
