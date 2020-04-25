3e9b8fa3741653869f3083c172df9a54a270c3af
Grenkin1988 <kruglovgleb@gmail.com>
2020-04-25 15:02:05 +0300

    Lesson 4.1 - Watch Where You’re Going

---
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody playerRigidbody;
+    private GameObject focalPoint;
+
+    [SerializeField]
+    private float moveSpped = 10;
+
+    private void Awake() {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Start() {
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    private void Update() {
+        float forward = Input.GetAxis("Vertical");
+        playerRigidbody.AddForce(focalPoint.transform.forward * moveSpped * forward);
+    }
+}
---
+++ Prototype 4/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class RotateCamera : MonoBehaviour {
+    [SerializeField]
+    private float rotationSpeed;
+
+    private void Start() {
+
+    }
+
+    private void Update() {
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontal * Time.deltaTime * rotationSpeed);
+    }
+}
