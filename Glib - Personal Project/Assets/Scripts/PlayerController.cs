0527369544ad1e410ac939a25d87c9f8d21b3dd7
Grenkin1988 <kruglovgleb@gmail.com>
2020-04-25 13:30:07 +0300

    Lab 3 - Player Control

---
+++ Glib - Personal Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField]
+    private float moveSpeed = 10;
+    [SerializeField]
+    private float xBound = 12;
+    [SerializeField]
+    private float zBound = 5;
+
+    private void Start() {
+
+    }
+
+    private void Update() {
+        MovePlayer();
+        ConstrainPlayerPosition();
+    }
+
+    private void MovePlayer() {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * Time.deltaTime * moveSpeed * horizontal);
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed * vertical);
+    }
+
+    private void ConstrainPlayerPosition() {
+        var position = transform.position;
+        float xPos = Mathf.Clamp(position.x, -xBound, xBound);
+        float zPos = Mathf.Clamp(position.z, -zBound, zBound);
+        transform.position = new Vector3(xPos, position.y, zPos);
+    }
+}
