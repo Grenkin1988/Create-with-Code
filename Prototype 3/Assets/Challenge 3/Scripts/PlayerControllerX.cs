f23f79c3e43267cc57c25ad07a01a8ff3de470b1
Grenkin1988 <kruglovgleb@gmail.com>
2020-04-25 13:04:57 +0300

    Challenge 3 - Balloons, Bombs, & Booleans

---
+++ Prototype 3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour {
+    public bool GameOver { get; private set; }
+
+    public float floatForce;
+    private float gravityModifier = 1.5f;
+    private Rigidbody playerRb;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem fireworksParticle;
+
+    private AudioSource playerAudio;
+    public AudioClip moneySound;
+    public AudioClip explodeSound;
+    public AudioClip floorSound;
+
+    [SerializeField]
+    private float hightLimit;
+    private bool IsLowEnough => transform.position.y < hightLimit;
+
+    private void Awake() {
+        playerRb = GetComponent<Rigidbody>();
+        playerAudio = GetComponent<AudioSource>();
+    }
+
+    // Start is called before the first frame update
+    private void Start() {
+        Physics.gravity *= gravityModifier;
+        // Apply a small upward force at the start of the game
+        playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        // While space is pressed and player is low enough, float up
+        if (Input.GetKey(KeyCode.Space) && !GameOver && IsLowEnough) {
+            playerRb.AddForce(Vector3.up * floatForce);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if (other.gameObject.CompareTag("Bomb")) {
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(explodeSound, 1.0f);
+            GameOver = true;
+            Debug.Log("Game Over!");
+            Destroy(other.gameObject);
+        } else if (other.gameObject.CompareTag("Money")) {
+            fireworksParticle.Play();
+            playerAudio.PlayOneShot(moneySound, 1.0f);
+            Destroy(other.gameObject);
+        } else if (other.gameObject.CompareTag("Ground")) {
+            playerAudio.PlayOneShot(floorSound, 1.0f);
+            playerRb.AddForce(Vector3.up * 15, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Prototype 3/Assets/Challenge 3/Scripts/SpawnManagerX.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour {
+    public GameObject[] objectPrefabs;
+    private float spawnDelay = 2;
+    private float spawnInterval = 1.5f;
+
+    private PlayerControllerX playerControllerScript;
+
+    // Start is called before the first frame update
+    private void Start() {
+        Invoke(nameof(SpawnObject), spawnDelay);
+        playerControllerScript = FindObjectOfType<PlayerControllerX>();
+    }
+
+    // Spawn obstacles
+    private void SpawnObject() {
+        if (playerControllerScript.GameOver) { return; }
+        // Set random spawn location and random object index
+        var spawnLocation = new Vector3(30, Random.Range(5, 15), 0);
+        int index = Random.Range(0, objectPrefabs.Length);
+
+        Instantiate(objectPrefabs[index], spawnLocation, objectPrefabs[index].transform.rotation);
+        Invoke(nameof(SpawnObject), spawnInterval);
+    }
+}
