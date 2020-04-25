946e2ad498aaa8c5c196cfda97524ae2f78a7da8
Grenkin1988 <kruglovgleb@gmail.com>
2020-04-25 18:48:58 +0300

    Lesson 5.1 - Clicky Mouse

---
+++ Prototype 5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    [SerializeField]
+    private List<GameObject> targets;
+    [SerializeField]
+    private float spawnRate = 1.0f;
+
+    private void Start() {
+        StartCoroutine(SpawnTarget());
+    }
+
+    private void Update() {
+
+    }
+
+    IEnumerator SpawnTarget() {
+        while (true) {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+}
---
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour {
+    private Rigidbody targetRb;
+
+    [SerializeField] private float minSpeed = 12;
+    [SerializeField] private float maxSpeed = 16;
+    [SerializeField] private float maxTorque = 10;
+    [SerializeField] private float xRange = 4;
+    [SerializeField] private float ySpawnPos = -6;
+
+    private void Awake() {
+        targetRb = GetComponent<Rigidbody>();
+    }
+
+    private void Start() {
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(
+            RandomTorque(),
+            RandomTorque(),
+            RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
+    }
+
+    private void Update() {
+
+    }
+
+    private void OnMouseDown() {
+        DestroyThis();
+    }
+
+    private void DestroyThis() { 
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        DestroyThis();
+    }
+
+    private Vector3 RandomForce() =>
+        Vector3.up * Random.Range(minSpeed, maxSpeed);
+
+    private float RandomTorque() =>
+        Random.Range(-maxTorque, maxTorque);
+
+    private Vector3 RandomSpawnPos() =>
+        new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+}
