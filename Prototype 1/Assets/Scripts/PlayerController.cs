fa01e1d2b3cfe7d7cd67429b3f6fd71c30850b29
Grenkin1988 <kruglovgleb@gmail.com>
2020-04-25 22:04:15 +0300

    Lesson 6.2 - Research and Troubleshooting

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -1,19 +1,66 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     [SerializeField]
-    private float _speed = 20;
+    private float _horsePower = 20;
     [SerializeField]
     private float _turnSpeed = 10;
 
-    private float _forwardInput = 20;
+    [SerializeField]
+    private Transform centerOfMass;
+
+    private float _forwardInput;
     private float _horizontalInput;
 
-    private void Update() {
-        _horizontalInput = Input.GetAxis("Horizontal");
-        _forwardInput = Input.GetAxis("Vertical");
+    private Rigidbody playerRigidbody;
 
-        transform.Translate(Vector3.forward * Time.deltaTime * _speed * _forwardInput);
-        transform.Rotate(Vector3.up, Time.deltaTime * _turnSpeed * _horizontalInput);
+    [SerializeField]
+    private TextMeshProUGUI speedometerText;
+    private float speed;
+
+    [SerializeField]
+    private TextMeshProUGUI rpmText;
+    private float rpm;
+
+    [SerializeField]
+    private List<WheelCollider> allWheels;
+    private int wheelsOnGround;
+
+    private void Awake() {
+        playerRigidbody = GetComponent<Rigidbody>();
+        playerRigidbody.centerOfMass = centerOfMass.position;
+    }
+
+    private void Update() {
+        if (IsOnGround()) {
+            _horizontalInput = Input.GetAxis("Horizontal");
+            _forwardInput = Input.GetAxis("Vertical");
+
+            transform.Rotate(Vector3.up, Time.deltaTime * _turnSpeed * _horizontalInput);
+        }
+        speed = Mathf.Round(playerRigidbody.velocity.magnitude * 2.237f);
+        speedometerText.text = $"Speed: {speed}mph";
+
+        rpm = Mathf.Round((speed % 30) * 40);
+        rpmText.text = $"RPM: {rpm}";
+    }
+
+    private void FixedUpdate() {
+        if (IsOnGround()) {
+            playerRigidbody.AddRelativeForce(Vector3.forward * _horsePower * _forwardInput);
+        }
+    }
+
+    private bool IsOnGround() {
+        wheelsOnGround = 0;
+        foreach (var wheel in allWheels) {
+            if (wheel.isGrounded) {
+                wheelsOnGround++;
+            }
+        }
+
+        return wheelsOnGround >= 2;
     }
 }
